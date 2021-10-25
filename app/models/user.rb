b11134cfa3577963a09ed71379d8096aad01e6f7
Nga <38340962+nga20167304@users.noreply.github.com>
2021-10-25 11:02:31 +0700

    active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :icon
+
+  def icon_thumbnail
+    if icon.attached?
+      icon.variant(resize: "150x150!").processed
+    else
+      ""
+    end
+  end
 end
