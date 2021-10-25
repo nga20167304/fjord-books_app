44c6a78ac66f5350757b49be77cbb23b74353970
Nga <38340962+nga20167304@users.noreply.github.com>
2021-10-25 14:47:58 +0700

    change icon to avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :icon
-
-  def icon_thumbnail
-    if icon.attached?
-      icon.variant(resize_to_fit: [150,150]).processed
-    end
-  end
 end
