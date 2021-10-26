5ddc357029ddee233a93d196ccf31ca6521eb32a
Nga <38340962+nga20167304@users.noreply.github.com>
2021-10-26 14:14:11 +0700

    change to human_attribute_name

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,8 @@ module BooksApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     #config i18n
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.available_locales = [:en, :ja]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :ja
   end
 end
