70ac037103b08a7f02f29aa8a43ccc1f6fb30be4
Nga <38340962+nga20167304@users.noreply.github.com>
2021-10-26 10:28:31 +0700

    user follow

---
+++ app/controllers/follow_relationships_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class FollowRelationshipsController < ApplicationController
+  before_action :set_user
+  def create
+    current_user.follow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+  end
+
+  def destroy
+    current_user.unfollow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+end
