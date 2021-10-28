ca16c166a93b79c08eb43671cd153a5be63f7d6f
Nga <38340962+nga20167304@users.noreply.github.com>
2021-10-28 20:19:01 +0700

    add user_id and comments controller

---
+++ app/controllers/books/comments_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Books::CommentsController < ApplicationController
+  def create
+    book = Book.find(params[:book_id])
+    comment = book.comments.build(comment_params)
+    comment.user = current_user
+
+    comment.save!
+    redirect_to book
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_26_112553) do
+ActiveRecord::Schema.define(version: 2021_10_28_131650) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -55,7 +55,9 @@ ActiveRecord::Schema.define(version: 2021_10_26_112553) do
     t.integer "commentable_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "relationships", force: :cascade do |t|
@@ -94,4 +96,5 @@ ActiveRecord::Schema.define(version: 2021_10_26_112553) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "users"
 end
