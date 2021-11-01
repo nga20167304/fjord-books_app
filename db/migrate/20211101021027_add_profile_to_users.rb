00d72978ff0aaf56cc5add4ac486b461d478fad7
Nga <38340962+nga20167304@users.noreply.github.com>
2021-11-01 09:13:28 +0700

    fix i18n

--- db/migrate/20211101021027_add_profile_to_users.rb
+++ db/migrate/20211101021027_add_profile_to_users.rb
@@ -1,7 +1,7 @@
 class AddProfileToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :postal_code, :string
-    add_column :users, :bio, :string
-    add_column :users, :address, :strinng
+    add_column :users, :self_introduction, :string
+    add_column :users, :address, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_01_010633) do
+ActiveRecord::Schema.define(version: 2021_11_01_021027) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -21,7 +21,19 @@ ActiveRecord::Schema.define(version: 2021_11_01_010633) do
     t.string "picture"
   end
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'strinng' for column 'address'
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "postal_code"
+    t.string "self_introduction"
+    t.string "address"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
