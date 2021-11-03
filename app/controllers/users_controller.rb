60b409c42670c4965033db47f05bc8271c13c4fe
Nga <38340962+nga20167304@users.noreply.github.com>
2021-11-03 21:25:55 +0700

    add i18n and following, followers page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,26 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :find_user, only: %i[show following follower]
+
   def index
     @users = User.with_attached_avatar.order(:id).page(params[:page])
   end
 
   def show
+  end
+
+  def following
+    @followings = @user.followings
+  end
+
+  def follower
+    @followers = @user.followers
+  end
+
+  private
+  
+  def find_user
     @user = User.find(params[:id])
   end
 end
