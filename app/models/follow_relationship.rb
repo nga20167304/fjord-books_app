50c944c42b1c7029b4002b3496383abe2376e751
Nga <38340962+nga20167304@users.noreply.github.com>
2021-11-10 07:01:57 +0700

    fix unique and format

--- app/models/follow_relationship.rb
+++ app/models/follow_relationship.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class FollowRelationship < ApplicationRecord
-  belongs_to :follower, class_name: 'User', unique: true
-  belongs_to :following, class_name: 'User', unique: true
+  belongs_to :follower, class_name: 'User'
+  belongs_to :following, class_name: 'User'
+
+  validates :following_id, :uniqueness => { :scope => :follower_id }
 end
