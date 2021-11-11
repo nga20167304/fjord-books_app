64aec6f75ac49c23addcf0cbee39f7504756726f
Nga <38340962+nga20167304@users.noreply.github.com>
2021-11-11 08:19:23 +0700

    fix after comment

--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -4,12 +4,18 @@ class FollowRelationshipsController < ApplicationController
   before_action :set_user
   def create
     current_user.follow(@user)
-    redirect_to @user
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 
   def destroy
     current_user.unfollow(@user)
-    redirect_to @user
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 
   private
