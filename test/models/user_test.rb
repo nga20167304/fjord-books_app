532bf10cffcb80d8f4e4cbb14ce3dba254d50ca2
Nga <38340962+nga20167304@users.noreply.github.com>
2021-11-17 08:59:07 +0700

    change models user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,6 +6,7 @@ class UserTest < ActiveSupport::TestCase
   setup do
     @user1 = FactoryBot.create(:user)
     @user2 = FactoryBot.create(:user)
+    @user1.follow(@user2)
   end
 
   test '#name_or_email' do
@@ -15,13 +16,22 @@ class UserTest < ActiveSupport::TestCase
     assert_equal @user1.email, @user1.name_or_email
   end
 
-  test '#follow/unfollow' do
-    assert_not @user1.following?(@user2)
-    assert_not @user2.followed_by?(@user1)
-    @user1.follow(@user2)
+  test '#following?' do
+    assert @user1.following?(@user2)
+    assert_not @user2.following?(@user1)
+  end
+
+  test '#followed_by?' do
+    assert @user2.followed_by?(@user1)
+    assert_not @user1.followed_by?(@user2)
+  end
+
+  test '#follow' do
     assert @user1.following?(@user2)
     assert @user2.followed_by?(@user1)
+  end
 
+  test '#unfollow' do
     @user1.unfollow(@user2)
     assert_not @user1.following?(@user2)
     assert_not @user2.followed_by?(@user1)
