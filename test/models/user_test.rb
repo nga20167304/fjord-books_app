23e81ad874801cee28129b8ec36504a4de7c4a56
Nga <38340962+nga20167304@users.noreply.github.com>
2021-11-20 18:42:12 +0700

    fix follow and unfollow model user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -31,11 +31,15 @@ class UserTest < ActiveSupport::TestCase
     @user1.follow(@user2)
     assert_equal @user1.followings.count, 1
     assert_equal @user2.followers.count, 1
+    assert @user2, @user1.followings
+    assert @user1, @user2.followers
   end
 
   test '#unfollow' do
     @user1.unfollow(@user2)
     assert_equal @user1.followings.count, 0
     assert_equal @user2.followers.count, 0
+    assert [], @user1.followings
+    assert [], @user2.followers
   end
 end
